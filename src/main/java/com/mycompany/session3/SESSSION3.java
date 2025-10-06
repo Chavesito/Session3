91fd161f2c2775e8754cad278893c2b173405a3e
Chavez Leonardo <leo.favio2003@gmail.com>
2025-10-06 17:04:15 +0200

    Commit 1

---
+++ src/main/java/com/mycompany/session3/SESSSION3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.session3;
+
+/**
+ *
+ * @author Leonardo Chavez
+ */
+public class SESSSION3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/com/mycompany/session3/CalculadoraTest.java
@@ -0,0 +1,18 @@
+package com.mycompany.session3;
+
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Leonardo Chavez
+ */
+public class CalculadoraTest {
+    @AfterEach
+}
